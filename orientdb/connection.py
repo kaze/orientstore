0bfb6fcea0521bb14f78316a21bcabc4bfbd1d8d
zsolt kormany <zsoltkormany@gmail.com>
2015-01-14 20:41:44 +0100

    revert connection.py, because pyorient get its settings from constants, does not accept a string

--- orientdb/connection.py
+++ orientdb/connection.py
@@ -6,9 +6,9 @@ from config import settings
 db_client = pyorient.OrientDB(settings.DB_HOST, int(settings.DB_PORT))
 db_client.connect(settings.DB_USER, settings.DB_PASS)
 
-if not db_client.db_exists(settings.DB_NAME, settings.DB_STORAGE_TYPE):
+if not db_client.db_exists(settings.DB_NAME, pyorient.STORAGE_TYPE_PLOCAL):
     db_client.db_create(settings.DB_NAME,
-                        settings.DB_TYPE,
-                        settings.DB_STORAGE_TYPE)
+                        pyorient.DB_TYPE_GRAPH,
+                        pyorient.STORAGE_TYPE_PLOCAL)
 
 db_client.db_open(settings.DB_NAME, settings.DB_USER, settings.DB_PASS)
